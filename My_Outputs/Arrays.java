e51f8a89cb26c271a56b9961acdf7ab2d7780594
Karuu <carl.mendiola@umak.edu.ph>
2026-07-04 20:50:52 +0800

    Made some practice outputs for first part

--- My_Outputs/Arrays.java
+++ My_Outputs/Arrays.java
@@ -1,3 +1,10 @@
 public class Arrays {
-    
+    public static void main(String[] args) {
+        System.out.println("- Arrays Traversal -");
+
+        int arr[] = {5, 3, 8, 6, 2};
+        for (int k = 0; k < arr.length; k++) { //Gets all the contents of the array
+            System.out.print(arr[k] + " "); // prints the contents of the array
+        }   
+    }
 }
