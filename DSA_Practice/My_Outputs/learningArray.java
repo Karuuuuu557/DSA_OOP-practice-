d86c4a43f65fcbdb5838d2f8d7bf0fa58a3771b1
Karuu <carl.mendiola@umak.edu.ph>
2026-09-01 23:23:41 +0800

    new prog: largest element in an array getter

--- DSA_Practice/My_Outputs/learningArray.java
+++ DSA_Practice/My_Outputs/learningArray.java
@@ -1,14 +1,26 @@
 public class learningArray {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
+        int[] arr1 = {10, 20, 30, 40, 50};
         int sum = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-           sum += arr[i];
+        for (int i = 0; i < arr1.length; i++) {
+           sum += arr1[i];
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
         }
         System.out.println("Sum of array elements: " + sum);
+
+        int [] arr2 = {10, 25, 7, 42, 18};
+        int largest = arr2[0];
+
+
+        for (int i = 1; i <arr2.length; i++) {
+            if (arr2[i] > largest) {
+                largest = arr2[i];
+            }
+        }
+        System.out.println("");
+        System.out.println("Largest element in the array: " + largest);
     }
 }
