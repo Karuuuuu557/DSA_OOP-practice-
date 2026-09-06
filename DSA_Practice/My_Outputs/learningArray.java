57d208129ea9d0df4703864ecb89f3327b550039
Karuu <carl.mendiola@umak.edu.ph>
2026-09-06 22:11:01 +0800

    made code organize and putted notices to what program ran for cleaner output

--- DSA_Practice/My_Outputs/learningArray.java
+++ DSA_Practice/My_Outputs/learningArray.java
@@ -1,5 +1,6 @@
 public class learningArray {
     public static void main(String[] args) {
+        System.out.println("=== Program 1: Array Sum ===");
         int[] arr1 = {10, 20, 30, 40, 50};
         int sum = 0;
 
@@ -13,6 +14,8 @@ public class learningArray {
 
         System.out.println("Sum of array elements: " + sum);
 
+        System.out.println();
+        System.out.println("=== Program 2: Largest Element ===");
         int[] arr2 = {10, 25, 7, 42, 18};
         int largest = arr2[0];
 
@@ -22,9 +25,10 @@ public class learningArray {
             }
         }
 
-        System.out.println("");
         System.out.println("Largest element in the array: " + largest);
 
+        System.out.println();
+        System.out.println("=== Program 3: Smallest Element ===");
         int[] arr3 = {35, 12, 89, 4, 27};
         int smallest = arr3[0];
 
@@ -36,6 +40,8 @@ public class learningArray {
 
         System.out.println("Smallest element in the array: " + smallest);
 
+        System.out.println();
+        System.out.println("=== Program 4: Even and Odd Count ===");
         int[] arr4 = {12, 7, 20, 15, 8, 3, 10};
         int even = 0;
         int odd = 0;
@@ -51,6 +57,8 @@ public class learningArray {
         System.out.println("Number of even elements in the array: " + even);
         System.out.println("Number of odd elements in the array: " + odd);
 
+        System.out.println();
+        System.out.println("=== Program 5: Average ===");
         int[] arr5 = {10, 20, 30, 40, 50};
         double average = 0;
 
@@ -61,6 +69,8 @@ public class learningArray {
         average /= arr5.length;
         System.out.println("Average of array elements: " + average);
 
+        System.out.println();
+        System.out.println("=== Program 6: Search Element ===");
         int[] arr6 = {10, 25, 7, 42, 18};
         int target = 42;
 
@@ -71,6 +81,8 @@ public class learningArray {
             }
         }
 
+        System.out.println();
+        System.out.println("=== Program 7: Search with Flag ===");
         int[] arr7 = {15, 8, 23, 41, 6};
         int target2 = 20;
         boolean found = false;
@@ -86,18 +98,22 @@ public class learningArray {
         if (!found) {
             System.out.println("Element " + target2 + " not found in the array.");
         }
-        int [] arr8 = {12, 5, 27, 8, 31, 14, 3};
+
+        System.out.println();
+        System.out.println("=== Program 8: Compare with Limit ===");
+        int[] arr8 = {12, 5, 27, 8, 31, 14, 3};
         int limit = 10;
         int count = 0;
+
         for (int i = 0; i < arr8.length; i++) {
             if (arr8[i] > limit) {
-                System.out.println(arr8[i] + " is greater than " + limit);  
+                System.out.println(arr8[i] + " is greater than " + limit);
                 count++;
             } else {
                 System.out.println(arr8[i] + " is not greater than " + limit);
             }
-
         }
+
         System.out.println("Count of elements greater than " + limit + ": " + count);
     }
 }
