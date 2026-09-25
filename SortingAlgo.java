34d982e5c841b5bc1e821b8e507bd68761b363b5
Karuu <carl.mendiola@umak.edu.ph>
2026-09-25 23:53:45 +0800

    cleaning up more

--- SortingAlgo.java
+++ SortingAlgo.java
@@ -67,7 +67,7 @@ public class SortingAlgo {
         for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
 
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
